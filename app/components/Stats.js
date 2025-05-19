addf17a93976119c11f284355e2a1a7524b4c594
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-05-19 12:59:55 +0530

    Fix

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -13,27 +13,27 @@ const Stats = () => {
         </div>
 
         <div className='flex flex-wrap w-full h-fit justify-center items-center md:gap-4 gap-0 my-14'>
-          <div className='flex flex-col justify-start items-center md:w-[16%] w-[48%] my-2'>
+          <div className='flex flex-col justify-start items-center md:w-[17%] w-[48%] my-2'>
             <h1 className='md:text-3xl text-2xl font-bold'>100+</h1>
             <h1 className='md:text-xl text-base'>Completed Projects</h1>
           </div>
 
-          <div className='flex flex-col justify-start items-center md:w-[16%] w-[48%] my-2'>
+          <div className='flex flex-col justify-start items-center md:w-[17%] w-[48%] my-2'>
             <h1 className='md:text-3xl text-2xl font-bold'>200+</h1>
             <h1 className='md:text-xl text-base'>Happy Clients</h1>
           </div>
 
-          <div className='flex flex-col justify-start items-center md:w-[16%] w-[48%] my-2'>
+          <div className='flex flex-col justify-start items-center md:w-[17%] w-[48%] my-2'>
             <h1 className='md:text-3xl text-2xl font-bold'>5+</h1>
             <h1 className='md:text-xl text-sm'>Awards and Recognitions</h1>
           </div>
 
-          <div className='flex flex-col justify-start items-center md:w-[16%] w-[48%] my-2'>
+          <div className='flex flex-col justify-start items-center md:w-[17%] w-[48%] my-2'>
             <h1 className='md:text-3xl text-2xl font-bold'>10+</h1>
             <h1 className='md:text-xl text-base'>Years Experience</h1>
           </div>
 
-          <div className='flex flex-col justify-start items-center md:w-[16%] w-[48%] my-2'>
+          <div className='flex flex-col justify-start items-center md:w-[17%] w-[48%] my-2'>
             <h1 className='md:text-3xl text-2xl font-bold'>1,00,000+</h1>
             <h1 className='md:text-xl text-base'>Square Feet Designed</h1>
           </div>
