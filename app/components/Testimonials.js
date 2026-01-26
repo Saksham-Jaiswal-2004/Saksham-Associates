d152056012843b7f8571b1c74ea799ae15a660ed
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2026-01-26 23:47:07 +0530

    Testimonial Section Update

--- app/components/Testimonials.js
+++ app/components/Testimonials.js
@@ -8,10 +8,50 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import Link from "next/link";
 import { getTestimonials } from "../../lib/getTestimonials";
 import Image from "next/image";
+import { AnimatedTestimonials } from "./ui/animated-testimonials";
 
 const Testimonials = () => {
   const [testimonials, setTestimonials] = useState([]);
   const [formattedDates, setFormattedDates] = useState([]);
+
+  const testimonial = [
+    {
+      quote: "Fantastic job done by Mrs.Madhu Jaiswal.I'am too much happy with the new interiors. It's more than I expected. Carry on SAKSHAM ASSOCIATES",
+      name: "Saurav Banthia",
+      designation: "Business",
+      src: "/images/testImg-1.jpg"
+    },
+    {
+      quote: "Marvelous job done by Mrs.Madhu Jaiswal and entire team. The complete house interiorsn has been done very gracefully. Which is liked by each and everyone who's coming to my house. Infact all are coming to see and admire the house, people are appreciating and liking the interior very much. Thank you so much for your efforts.",
+      name: "Sheetal Dassani",
+      designation: "Model",
+      src: "/images/testImg-3.jpg"
+    },
+    {
+      quote: "I, Jemin Doshi, Founder of Napcloud Sofas and Mattresses representing Furniture and Mattresses category in BNI INCREDIBLEZ, am hereby pleased to write a testimonial for MRS.MADHU JAISWAL, who represents INTERIOR DESIGNER Category in BNI INCREDIBLEZ. Madhu ji has a curios mind which helps find custom solutions for challenging engagements. The ethos of giver's gain is reflected in her when she referred me to one of her clients immediately in our very first meeting. I am grateful for the opportunity and trust I did it justice. She is one of the most balanced persons I have come across. She has good command over her subject and business along with great energy and perseverance towards perfection and great delivery. She is hence an ideal ambassador for our chapter. She knows the key important deliverable of her business and I am impressed with that. She has command over her subject and has proved to get the right product subscribed as per requirement of the clients. I am looking forward to staying associated with her and her guidance. Wishing them luck and success in all her future endeavors.",
+      name: "Jemin Doshi",
+      designation: "Napcloud Sofa & Mattresses",
+      src: "/images/testImg-2.jpg"
+    },
+    {
+      quote: "I, Megha Sarayan, owner of Land of Cakes, am pleased to write a testimonial for MRS.MADHU JAISWAL, Saksham Associates. Recently, I shifted my Chinar Park kitchen, and I availed myself of her services. I'm very satisfied with her services, as well as the format and discipline of work and execution and payment terms. I found her extremely professional, and her nature is very warm that adds to her aura. I've recommended her to all my friends and family and I wish her all the best for all her future endeavours.",
+      name: "Megha Sarayan",
+      designation: "Land of Cakes, Flavour The Savour",
+      src: "/images/testImg-2.jpg"
+    },
+    {
+      quote: "I get connected with MRS.MADHU JAISWAL,owner of SAKSHAM ASSOCIATES at BNI PLATFORM as INTERIOR DESIGNER category. She is consummate professional and responsive. Me and my wife completely trust her judgement and have come to greatly appreciate her unique ability to make living spaces both highly functional and aesthetically pleasing. She is cognizant of budget constraints and thoughtful about designs plan that maximise value.",
+      name: "Sushil Kumar Modi",
+      designation: "HANSAL HOLIDAYS TOURS & TRAVELS",
+      src: "/images/testImg-2.jpg"
+    },
+    {
+      quote: "I, Keshav Binani, owner of Grand Events, representing Wedding Planner category in BNI INCREDIBLEZ. I am hereby pleased to write a testimonial for MRS.MADHU JAISWAL. She has a positive attitude towards business and life. I had given her a connect which was not for a very large ticket, but the time taken to complete the project was less than the time taken by client to explain her requirements. I had also received commendable compliment from the client. So this is how one grows bigger and better gaining trust and delivering in time. Very much commendable and appreciated work done in no time. Wishing her all the good luck for her future endeavours.",
+      name: "Keshav Binani",
+      designation: "Grand Events (Wedding Planner)",
+      src: "/images/testImg-2.jpg"
+    }
+  ];
 
   useEffect(() => {
     async function fetchTestimonials() {
@@ -48,15 +88,17 @@ const Testimonials = () => {
   }, []);
 
   return (
-    <div className="h-screen my-16 flex flex-col items-center">
+    <div className="min-h-screen h-fit my-16 flex flex-col items-center">
       <div className="w-full flex justify-start">
         <h1 className="md:text-6xl text-4xl my-10 head">
           Stories of Inspired Spaces
         </h1>
       </div>
 
+      <AnimatedTestimonials testimonials={testimonial} />
+
       <div className="md:h-3/5 h-fit w-[90%] flex flex-col justify-center items-center my-10 mx-4">
-        <Swiper
+        {/* <Swiper
           // loop={true}
           loop={testimonials.length > 3}
           spaceBetween={30}
@@ -139,7 +181,7 @@ const Testimonials = () => {
               </Link>
             </SwiperSlide>
           ))}
-        </Swiper>
+        </Swiper> */}
 
         <div className="w-full flex justify-end my-4 mx-3">
           <Link
